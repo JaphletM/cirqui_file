dfd573ad273979363d66d0443a74031110c2dfa8
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-05-28 12:42:40 +0200

    feat: embedding- en Qdrant-configuratie toegevoegd
    Fixes #20

--- src/EmbeddingService.py
+++ src/EmbeddingService.py
@@ -1,6 +1,12 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-client = OpenAI()
+load_dotenv()
+
+client = OpenAI(
+api_key = os.getenv("OPENAI_API_KEY")
+)
 
 
 def embed_text(text):
